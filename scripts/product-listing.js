38b6efbb651f92467dcd63b189a6ce6ea7eab437
Angie Liceth <liceth5262@gmail.com>
2024-07-09 20:35:32 -0500

    filtracion de botones y pintadas de cards en pagina listado de productos en 70%

--- scripts/product-listing.js
+++ scripts/product-listing.js
@@ -1,29 +1,77 @@
-import { cards } from "../modules/data.js";
-import { pintarCards, 
-  agregarBotonesDeFiltrado,
-  ejecutarFiltros
+import {
+  filtrarCardsPorCategoria,
+  obtenerCategorias,
+  obtenerCards,
+  obtenerCardsPorId,
+} from "../modules/services.js";
 
-} from "../modules/ui.js";
-console.log(cards);
+//Filtrado de categorias
 
-const contenedorFiltros = document.getElementById("filtros");
-const listaCategorias = cards.map((cards) => cards.categoria);
-const categorias = Array.from(new Set(listaCategorias));
-const contenedorCards = document.getElementById("coleccion");
+const pintarCategorias = (listaCategorias) => {
+  listaCategorias.forEach((categorias) => {
+    const item = document.createElement("li");
+    const boton = document.createElement("button");
+    
+    boton.textContent = categorias.nombre;
 
-agregarBotonesDeFiltrado(categorias, contenedorFiltros, cards, contenedorCards);
+    boton.addEventListener("click", async () => {
+      const cardsFiltrados = await filtrarCardsPorCategoria(categorias.nombre);
+      pintarCards(cardsFiltrados)
+    });
 
-pintarCards(cards, contenedorCards);
 
-const botonTodos = document.getElementById("todos");
-botonTodos.addEventListener("click", (evento) => {
-  ejecutarFiltros(
-    evento.target.id,
-    contenedorFiltros.children,
-    cards,
-    contenedorCards
-  );
-});
+    item.appendChild(boton);
+    contenedorFiltros.appendChild(item);
+    
+  });
+}
+
+function pintarCards(listaCards, contenedor) {
+  const contenedorCards = contenedor? contenedor: document.getElementById("coleccion");
+  if (listaCards.length > 0) { 
+  contenedorCards.innerHTML = "";
+
+  listaCards.forEach((cards) => {
+    const tarjeta = document.createElement("article");
+    const figure = document.createElement("figure");
+    const imagen = document.createElement("img");
+    const section = document.createElement("section");
+    const h3 = document.createElement("h3");
+
+    imagen.setAttribute("src", cards.imagen);
+    imagen.setAttribute("alt", cards.nombre);
+    h3.textContent = cards.nombre;
+    
+
+    figure.innerHTML = `
+    <figure>
+        <img src=${cards.imagen} alt=${cards.nombre}>
+      </figure>
+    `;
+
+    section.innerHTML = `
+    <div>
+      <h3>${cards.nombre}</h3>
+      <p>${cards.precio}</p>
+    </div>
+    `;
+
+    tarjeta.addEventListener("click", () => {
+      //sessionStorage.setItem("idProducto", producto.id);
+      window.location.href = "/pages/product-details.html";
+    });
+
+    tarjeta.classList.add("card");
+
+    tarjeta.appendChild(figure);
+    tarjeta.appendChild(section);
+    contenedorCards.appendChild(tarjeta)
+  });
+
+} else
+  contenedorCards.textContent = "Producto no disponible";
+
+}
 
 const formularioBusqueda = document.getElementById("busqueda");
 
@@ -37,26 +85,15 @@ formularioBusqueda.addEventListener("submit", (evento) => {
   }
 });
 
-function busquedaCardsPorNombre(listaCards, nombreABuscar) {
-  return listaCards.filter((cards) =>
-    cards.nombreJoya.toLowerCase().includes(nombreABuscar.toLowerCase())
-  );
-}
 
-// Orden por precio
+//variables/constantes
+const contenedorFiltros = document.getElementById("filtros");
+const contenedorCards = document.getElementById("coleccion");
 
-const botonOrdenar = document.getElementById("ordenamiento");
-botonOrdenar.addEventListener("click", ordenarCards);
-
-function ordenarCards() {
-  const selector = document.getElementById("ordenamiento");
-  const valorSeleccionado = selector.value;
-
-  if (valorSeleccionado === "ascendente") {
-    cards.sort((a, b) => a.precio - b.precio);
-  } else if (valorSeleccionado === "descendente") {
-    cards.sort((a, b) => b.precio - a.precio);
-  }
-
-  pintarCards(cards, contenedorCards);
-}
+//ejecucion
+document.addEventListener("DOMContentLoaded", async () => {
+  const categorias = await obtenerCategorias();
+  const cards = await obtenerCards();
+  pintarCategorias(categorias, contenedorFiltros);
+  pintarCards(cards);
+});
